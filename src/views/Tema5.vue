aac6e3b8b6502b0744e7b2c08a4d709c18a0c1e0
Liborio Castañeda Valencia <ldcastaneda06@misena.edu.co>
2024-04-20 12:23:08 -0500

    Terminación de tema 6 y 7

--- src/views/Tema5.vue
+++ src/views/Tema5.vue
@@ -5,68 +5,166 @@
     .titulo-principal.color-acento-contenido
       .titulo-principal__numero 
         span 5
-      h1 Medicamentos
+      h1 Almacenamiento
     
-    .row
-      .col-md-7    
-        .cajon-b.ajuste_cajon_b.color-acento-contenido.p-3.mb-5
-          p.mb-0 Los medicamentos utilizados en la producción de especies pecuarias tienen importancia en el manejo y bienestar animal, pero siempre supervisado por un veterinario y bajo los parámetros técnicos de la norma. Por eso, a continuación, se describen las familias de los medicamentos utilizados de acuerdo a su función:
-            |
-            br
-      .col-md-5 
+
+    .row.justify-content-center.align-items-center.mb-5
+      .col-lg-5
         figure
-          img.img-t(src='@/assets/curso/images/tema5/img_1.svg', alt='')
-    
-    .row.justify-content-center.align-items-center.mb-5.mt-3 
-      .col-md-10
-        .tabla-b.color-acento-contenido
-          .tabla-b__header
-            h4.mb-0 Medicamentos manejo pecuario
-          table
-            tr
-              th Antiinflamatorios
-              td Son esteroidales y se utilizan para rebajar la inflamación, los esteroides exógenos (son inyectables) reducen la capacidad del cuerpo para combatir la infección y desarrollar inmunidad. Se utilizan principalmente para problemas artríticos y de cojeras entre otros.
-            tr
-              th Hormonas
-              td Por lo general son hormonas sintéticas que tienen efectos específicos sobre la actividad de otros órganos y muy utilizados para la actividad reproductiva de las especies.
-            tr
-              th Analgésicos
-              td Son medicamentos que producen un estado de conciencia inalterada, con supresión del dolor y sin pérdida de la capacidad sensorial. Estos se encuentran en las familias de los narcóticos (Fentanil, Meperidina, Etorfina, Metadona, Propoxifeno, Pentazocina y Butorfanol) y no narcóticos (Salicilatos, Índoles, Oxicanos, Ácidos orgánicos, Paraaminofenoles y Pirazolonas).
-            tr
-              th Antihistamínicos
-              td Son medicamentos que producen un estado de conciencia inalterada, con supresión del dolor y sin pérdida de la capacidad sensorial. Estos se encuentran en las familias de los narcóticos (Fentanil, Meperidina, Etorfina, Metadona, Propoxifeno, Pentazocina y Butorfanol) y no narcóticos (Salicilatos, Índoles, Oxicanos, Ácidos orgánicos, Paraaminofenoles y Pirazolonas) 
-            tr
-              th Antiparasitarios
-              td Parásitos internos y se utilizan de acuerdo al tipo de parásito que afecten, en el caso de los antihelmínticos son sustancias que actúan contra los parásitos internos de los animales domésticos, pero hay otros que actúan controlando (Nemátodos/redondos), (cestodos o planos segmentados) y parásitos planos no segmentados (Tremátodos).
-            tr
-              th Antiparasitarios externos
-              td Estos productos se utilizan para controlar moscas, garrapatas, nuches, son llamados insecticidas.
-   
+          img.img-t(src='@/assets/curso/images/tema3/img_1.png', alt='')
+      .col-lg-7.mb-3
+        p Al igual que las demás actividades de poscosecha, el almacenamiento tiene el objetivo de alargar la vida útil y conservar la calidad de los productos, específicamente la calidad comercial, mediante técnicas que permiten regular la respiración y la pérdida de agua (transpiración) los cuales son factores de deterioro de la calidad, que se agudizan bajo condiciones ambientales extremas porque se aceleran los procesos fisiológicos.
+        .tarjeta.color-secundario-claro-sin-bordes.p-3.ps-4.bg_18.mb-4.mb-lg-0
+          .row
+            .col-md-2
+              img.mt-3.img-t.m-auto(src='@/assets/curso/images/tema3/img_2.svg', alt='')
+            .col-md-10
+              p.mt-3.p-1 Adicionalmente, el almacenamiento permite estabilizar y regular los procesos de oferta y demanda, debido a que se da un control en la comercialización, al guardar producto para aprovisionar en el tiempo de no cosecha.
+      p.mt-1 No obstante, como lo indica Borrero Ortiz, M., & Urrea López, M. (2007), se debe tener presente y proveer las condiciones adecuadas durante el almacenamiento para evitar la continuidad de los procesos desarrollo propios de los productos, como, por ejemplo, enraizamiento, germinación de semillas, enverdecimiento, brotación y crecimiento o alargamiento.
+        span.text-bold  A continuación, se exponen algunos procesos metabólicos que se presentan después de la poscosecha:
+          
+    TabsC.color-primario
+      .py-3.py-md-4(titulo="Brotado")
+        .row
+          .col-md-6
+            figure
+              img(src='@/assets/curso/images/tema5/img_3.png', alt='Texto que describa la imagen')
+          .col-md-6.mb-4.mb-md-0
+            h4.mt-5 Brotado
+            p Causa deterioro especialmente en cebollas, ajo y papas, está relacionado con la latencia debida a factores externos e internos.
+        
+       
+      .py-3.py-md-4(titulo="Alargamiento")
+        .row
+          .col-md-6
+            figure
+              img(src='@/assets/curso/images/tema5/img_3.png', alt='Texto que describa la imagen')
+          .col-md-6.mb-4.mb-md-0
+            h4.mt-5 Alargamiento
+            p Se genera un alargamiento de las estructuras existentes. Se presenta en zanahorias, espárragos y nabos.
+        
+      .py-3.py-md-4(titulo="Enraizamiento")
+        .row
+          .col-md-6
+            figure
+              img(src='@/assets/curso/images/tema5/img_3.png', alt='Texto que describa la imagen')
+          .col-md-6.mb-4.mb-md-0
+            h4.mt-5 Enraizamiento
+            p Puede presentarse por condiciones de humedad elevada. Genera descomposición rápida, arrugamiento y agotamiento de reservas alimenticias en tubérculos y raíces
+        
+      .py-3.py-md-4(titulo="Enverdecimiento")
+        .row
+          .col-md-6
+            figure
+              img(src='@/assets/curso/images/tema5/img_3.png', alt='Texto que describa la imagen')
+          .col-md-6.mb-4.mb-md-0
+            h4.mt-5 Enverdecimiento
+            p La luz durante el almacenamiento produce un tejido verde, generando problemas de presentación, mal sabor y rechazo por parte del consumidor.
+        
+      .py-3.py-md-4(titulo="Germinación")
+        .row
+          .col-md-6
+            figure
+              img(src='@/assets/curso/images/tema5/img_3.png', alt='Texto que describa la imagen')
+          .col-md-6.mb-4.mb-md-0
+            h4.mt-5 Germinación
+            p En el transcurso del almacenamiento se favorece la germinación de semillas de los frutos maduros.
+        
+      .py-3.py-md-4(titulo="Endurecimiento")
+        .row
+          .col-md-6
+            figure
+              img(src='@/assets/curso/images/tema5/img_3.png', alt='Texto que describa la imagen')
+          .col-md-6.mb-4.mb-md-0
+            h4.mt-5 Endurecimiento
+            p La gravedad y la luz pueden ocasionar el encorvamiento de los tejidos. Los productos deformes son difíciles de empacar y de baja calidad comercial debido a la mala presentación.
+        
     .row.mb-4
       .col-auto
         img(src='@/assets/curso/images/tema5/img_7.svg' , alt='')
       .col-lg-6
-        h3.mt-4(data-aos="fade-right") Equipos, instrumental e insumos
+        h3.mt-4(data-aos="fade-right") Técnicas y condiciones 
   
     .row.align-items-center.mb-4
-      .col-lg-6(data-aos="fade-right")
-        p Son elementos básicos y necesarios que se deben poseer en una explotación pecuaria y que varían de acuerdo a la actividad y especie animal. Inicialmente el formato de registro (información individual y colectiva de los animales con sus producciones o los EPP (Elementos de Protección Personal) son insumos primordiales dentro del manejo animal.
-        .cajon.color-secundario.p-4.ps-5.bg_18.mb-4.mb-lg-0
-          p.m-0 Otros insumos que cabe mencionar son el guardián colector y bolsa recolectora con sus respectivos colores de acuerdo con la reglamentación vigente el cual es importante para depositar materiales peligrosos. Una nevera portátil será de gran utilidad para el transporte de vacunas o muestras y jeringas desechables de acuerdo con su acción junto con un botiquín de primeros auxilios.
+      .col-lg-7(data-aos="fade-right")
+        p Existen diversas técnicas de almacenamiento, entre las que se destacan el almacenamiento denominado común, donde los productos se conservar bajo condiciones ambientales naturales, durante el tiempo que se mantengan las características propias (sensoriales, nutricionales, físicas, químicas) o se produzcan alteraciones mínimas, ya que existen frutas y hortalizas como la papa, ajo y cítricos que se mantienen sin ningún tratamiento por un tiempo prolongado. Para lo cual se puede aprovechar la ventilación natural y así asegurar unas mejores condiciones. Pero, esta técnica de almacenamiento es arriesgada porque no existe control sobre el ambiente (temperatura y humedad)
       .col-lg-5.mb-4.mb-lg-0(data-aos="fade-left")
         figure
           img.img-t.m-auto(src='@/assets/curso/images/tema5/img_2.svg', alt='')
-      
+        
+    .row.align-items-center.mb-4
+      .col-lg-3(data-aos="fade-right")
+        figure
+          img.img-t.m-auto(src='@/assets/curso/images/tema5/img_2.svg', alt='')
+      .col-lg-9.mb-4.mb-lg-0(data-aos="fade-left")
+        .cajon.color-acento-contenido.p-4.ps-5.bg_18.mb-4.mb-lg-0
+          p.m-0 Otros insumos que cabe mencionar son el guardián colector y bolsa recolectora con sus respectivos colores de acuerdo con la reglamentación vigente el cual es importante para depositar materiales peligrosos. Una nevera portátil será de gran utilidad para el transporte de vacunas o muestras y jeringas desechables de acuerdo con su acción junto con un botiquín de primeros auxilios.
+        p Pero se debe tener presente, que una inapropiada utilización de las bajas temperaturas genere el efecto contrario a lo deseado, es decir aligera la degradación de la calidad de los productos, ya que una exposición prolongada al frío genera congelamiento y por ende formación de cristales que afectan la estructura porque se destruyen los tejidos de forma permanente, reflejado a la hora de descongelarse signos como:
+        
+        .row 
+          .col-md-4
+            ul.lista-ul--separador
+              li 
+                i.fas.fa-angle-right
+                | Deshidratación.
+              li 
+                i.fas.fa-angle-right
+                | Pérdida de líquidos.
+              li 
+                i.fas.fa-angle-right
+                | Alteración en los tejidos. 
+              li 
+                i.fas.fa-angle-right
+                | Desorden fisiológico.
+          .col-md-4
+            ul.lista-ul--separador
+              li 
+                i.fas.fa-angle-right
+                | Decoloración interna y externa.
+              li 
+                i.fas.fa-angle-right
+                | Falta de sabor.
+              li 
+                i.fas.fa-angle-right
+                | Ausencia o maduración no uniforme.
+              li 
+                i.fas.fa-angle-right
+                | Entre otros.
+          .col-md-4 
+            img.m-auto.w-50(src='@/assets/curso/images/tema3/img_2.svg', alt='')
+          p Otra técnica de almacenamiento es por medio de atmósferas controladas o modificadas, las cuales permiten adicionar, extraer o cambiar la concentración de gases que la componen, como son el dióxido de carbono (CO2), oxígeno (O2), etileno (C2H4), nitrógeno (N), con la finalidad de generar un retraso en los cambios fisiológicos, biológicos y químicos propios del ciclo de vida de los productos, como en la velocidad de respiración, la generación de etileno y la aparición de ablandamiento y variación en la composición, además, se minimiza la susceptibilidad a los daños causados por patógenos.
 
-    .cajon.bg_color_acento_contenido.p-3.mb-5(data-aos="zoom-in-up")
-      .row.justify-content-center.align-items-star.p-3
-        .col-md-6
-          figure
-            img(src='@/assets/curso/images/tema5/img_3.png', alt='Texto que describa la imagen')
-        .col-md-6.mb-4.mb-md-0
-          p.mb-4 Otros elementos básicos son los de realizar labores de mecánica, labranza y sujeción entendiendo que todos estos equipos e instrumentales estén en buen estado, desinfectados, limpios y que sean los apropiados para cada caso. 
-          p.mb-4 Finalmente, los insumos dependen del área de alimentación en los que se debe tener por fuente de proteína u otro (cereales, soya) o los de otra área como la de producción agrícola en la cual tendremos como insumo los fertilizantes. Así mismo, habrá otras áreas como producción de acuerdo con el tipo de animal. Es importante que los productores de acuerdo con la proyección y la utilidad determinen la cantidad y utilidad del insumo.
+    .row.align-items-center.mb-4
+      .col-lg-5.mb-4.mb-lg-0(data-aos="fade-left")
+        figure
+          img.img-t.m-auto(src='@/assets/curso/images/tema5/img_2.svg', alt='')
+      .col-lg-7(data-aos="fade-right")
+        p Cada gas empleado en la atmósfera cumple su función y sus niveles varían de acuerdo a los beneficios o consecuencias que generan, por ejemplo, el oxígeno no están deseado ya que genera oxidación por tanto el nitrógeno se encarga de desplazar, además de que ayuda a minimizar el crecimiento de patógenos; por su parte el dióxido de carbono como disminuye la respiración y frena la acción de metileno hace que los órganos entren en un estado de reposo que favorece porque frena y suspende todos los procesos ayudando a la conservación. Pero, al igual que la temperatura, cuando la concentración de gases no es la propicia, se generan consecuencias negativas, como presencia de sabores y olores desagradables, fermentación y asfixia de tejidos.
+    
+    .row.mb-4
+      .col-auto
+        img(src='@/assets/curso/images/tema5/img_7.svg' , alt='')
+      .col-lg-6
+        h3.mt-4(data-aos="fade-right") Variables
+  
+    .row.justify-content-center.align-items-center.mb-5
+      p Existen diversas variables y factores que influyen en el proceso como son las condiciones con que ingresa el producto al almacenamiento, porque la calidad de la conservación se ve afectada por daños físicos, mecánicos, biológicos, químicos. Se requiere que las condiciones de temperatura y humedad estén monitoreadas, controladas y permanezcan constantes durante el ciclo de almacenamiento debido a que variaciones mínimas tanto de aumento como de disminución pueden generar efectos negativos. 
+      .col-lg-7.mb-3
+        p Otra variable que tiene influencia en el proceso son las condiciones de higiene y desinfección en los lugares de almacenamiento, por lo que se requiere constantemente realizar limpieza para evitar la proliferación de bacterias, hongos, moho y demás microorganismos que afecten la calidad del producto y puede ocasionar problemas en la salud de los consumidores. 
+        .tarjeta.color-secundario-claro-sin-bordes.p-3.ps-4.bg_18.mb-4.mb-lg-0
+          .row
+            .col-md-3
+              img.mt-3.img-t.m-auto(src='@/assets/curso/images/tema3/img_2.svg', alt='')
+            .col-md-9
+              p.mt-3.p-1 Además, estos espacios o bodegas deben de ser diseñados de tal forma que haya buena circulación de aire y a la hora de almacenar se acomode organizadamente, teniendo presente la rotación de los productos, sin ir a pegar a la pared, ni al techo, ni poner directamente en el piso, se debe utilizar estibas o plataformas y dejar distancia entre arrume y arrume, entre arrume y pared y entre arrume y techo.
+      .col-lg-5
+        figure
+          img.img-t(src='@/assets/curso/images/tema3/img_1.png', alt='')
 
+      .col-md-12  
+        .cajon.color-acento-contenido.p-4.ps-5.bg_18.mb-4.mb-lg-0
+          p.m-0 De igual forma se debe usar medidas de seguridad y elementos de protección personal porque existen riesgos asociados como golpes y sepultamiento por caídas de cargas.
+        
 
   </template>
 
--- src/views/Tema6.vue
+++ src/views/Tema6.vue
@@ -5,74 +5,125 @@
     .titulo-principal.color-acento-contenido
       .titulo-principal__numero 
         span 6
-      h1 Tratamientos alternativos
+      h1 Trazabilidad
 
+    .row.justify-content-center.align-items-center.mb-5
+      .col-lg-4
+        figure
+          img.img-t(src='@/assets/curso/images/tema3/img_1.png', alt='')
+      .col-lg-8.mb-3
+        .cajon.color-acento-contenido.p-4.ps-5.bg_18.mb-5.mb-lg-0
+          p.m-0 Actualmente debido a las crisis alimentarias y la aparición de enfermedades zoonóticas, ha aumentado el cuidado y prevención por parte de los consumidores, generando que el sector agroalimentario se preocupe y se enfoque en asegurar a los consumidores la calidad de los productos ofrecidos, mediante la implementación de medidas de carácter obligatorio, como también disposiciones voluntarias como la trazabilidad. 
+        p.mt-2 Debido a que la trazabilidad permite conocer el origen de los productos y hacerle seguimiento a lo largo de todo el proceso poscosecha hasta llegar al comprador final, lo que beneficia la seguridad alimentaria y da garantía al consumidor, pues existen registros y rastro de donde viene el producto, por donde paso y a donde llegó, es decir como lo indica Sevilla, E., Ortiz, F., & López, M. (2013), permite obtener una trazabilidad ascendente, descendente y total o también denominada hacia atrás, hacia adelante e interna. En el caso de presentarse errores, estos se pueden detectar y saber dónde se originó para así tomar las medidas correctivas necesarias y garantizar la inocuidad de los productos.
     
     .row.justify-content-center.align-items-center.mb-5
-      .col-lg-12.mb-2
-        .bloque-texto-g.p-3.p-sm-4.p-md-5
-          .bloque-texto-g__img(
-            :style="{'background-image': `url(${require('@/assets/curso/images/tema6/img_1.png')})`, 'width': '100%', 'height':'249px'}"
-          )
-      .col-lg-9.mb-5
-        .bloque-texto-g__texto.p-4.jumbotron-16(
-          :style="{'top': '55px', 'position': 'relative', 'width':'100%'}"
-        )
-          p.mb-0.text-white Los tratamientos utilizados en las producciones pecuarias son implementados como una alternativa de manejo animal y/o minimizar gastos de producción. Encontrando:
+      .col-lg-12
+        .titulo-sexto.color-acento-contenido.mb-3
+          h5 Figura 6.
+          span Sentidos de la trazabilidad
+        figure
+          img.img-t.mb-2(src='@/assets/curso/images/tema6/img_6.svg', alt='')
+
+    .row.mb-4
+      .col-auto
+        img(src='@/assets/curso/images/tema5/img_7.svg' , alt='')
+      .col-lg-6
+        h3.mt-4(data-aos="fade-right") Importancia y responsabilidades
 
     .row.justify-content-center.align-items-center.mb-5
-      .col-lg-6
-        .col-lg-12
-          .tarjeta-avatar-b.mb-4
-              .tarjeta-avatar-b__img
-                img.img-t.m-auto(src='@/assets/curso/images/tema6/img_2.svg', alt='')
-              .tarjeta.tarjeta.color_secundario_tarjetas.shadow-sm
-                .p-4
-                  p.text-small
-                    span.text-bold La homeopatía 
-                    | es una medicina alternativa que ha cogido auge en los países desarrollados y ahora es tomado en cuenta por países como Colombia. Es una práctica terapéutica que armoniza algunos extractos de plantas del entorno para sanar el animal.
-        
-        .col-lg-12
-          .tarjeta-avatar-b.mb-4
-              .tarjeta-avatar-b__img
-                img.img-t.m-auto(src='@/assets/curso/images/tema6/img_3.svg', alt='')
-              .tarjeta.tarjeta.color_secundario_tarjetas.shadow-sm
-                .p-4
-                  p.text-small De igual manera, 
-                    span.text-bold la acupuntura 
-                    | es un método que se realiza mediante la utilización de agujas beneficiosas y cuyo objetivo es la postura de agujas en el sitio de la dolencia.
-        
-        .col-lg-12
-          .tarjeta-avatar-b.mb-4
-              .tarjeta-avatar-b__img
-                img.img-t.m-auto(src='@/assets/curso/images/tema6/img_4.svg', alt='')
-              .tarjeta.tarjeta.color_secundario_tarjetas.shadow-sm
-                .p-4
-                  p.text-small De igual manera, 
-                    span.text-bold la acupuntura 
-                    | es un método que se realiza mediante la utilización de agujas beneficiosas y cuyo objetivo es la postura de agujas en el sitio de la dolencia.
-        
-        .col-lg-12
-          .tarjeta-avatar-b.mb-4
-              .tarjeta-avatar-b__img
-                img.img-t.m-auto(src='@/assets/curso/images/tema6/img_5.svg', alt='')
-              .tarjeta.tarjeta.color_secundario_tarjetas.shadow-sm
-                .p-4
-                  p.text-small De igual manera, 
-                    span.text-bold la acupuntura 
-                    | es un método que se realiza mediante la utilización de agujas beneficiosas y cuyo objetivo es la postura de agujas en el sitio de la dolencia.
-        
-      .col-lg-6
+      .col-lg-5
+        figure
+          img.img-t(src='@/assets/curso/images/tema3/img_1.png', alt='')
+      .col-lg-7.mb-3
+        p.mt-2 Por medio del seguimiento y rastreabilidad se tiene la opción de intervenir sobre los factores que permitan mejorar la calidad del producto final, la seguridad de los productos y la transparencia de los mercados. Por tanto, se debe tener toda la información relacionada de cada uno de los eslabones de la cadena:
+    
+    p.text-bold.text-center A continuación, se representan los ámbitos o elementos involucrados en la trazabilidad:
+    
+    .row.justify-content-center.align-items-center.mb-5
+      .col-lg-12
         .titulo-sexto.color-acento-contenido.mb-3
-          h5 Figura 4.
-          span  Tratamientos alternativos de producción pecuaria 
+          h5 Figura 7.
+          span Ámbitos de la trazabilidad
         figure
           img.img-t.mb-2(src='@/assets/curso/images/tema6/img_6.svg', alt='')
           figcaption 
-            p
-              span.text-bold Nota. 
-              | Son tratamientos utilizados para animales más felices y más sano que conllevan a una mejor producción.
+            span.text-bold Nota. 
+            | Sevilla, E., Ortiz, F., & López, M. (2013)
+               
+    .row.justify-content-center.align-items-center.mb-0
+      .col-lg-12.mb-5
+        .bloque-texto-g.color-primario.p-3.p-sm-4.p-md-5
+          .bloque-texto-g__img(
+            :style="{'background-image': `url(${require('@/assets/curso/images/tema3/img_3.png')})`}"
+          )
+          .bloque-texto-g__texto.p-4
+            p.mb-0 Además, la trazabilidad es de gran importancia, porque como se muestra en la siguiente figura, funciona como un instrumento de información para todos los actores de la cadena producción, generando beneficios para los consumidores, la empresa y los entes de control, ya que permite obtener un control del flujo del producto y un análisis de riesgos que incluye la determinación, gestión, control y comunicación.
+  
+    .row.justify-content-center.align-items-center.mb-5
+      .col-lg-12
+        .titulo-sexto.color-acento-contenido.mb-3
+          h5 Figura 8.
+          span Importancia y beneficios de la trazabilidad
+        figure
+          img.img-t.mb-2(src='@/assets/curso/images/tema6/img_6.svg', alt='')
+          figcaption 
+            span.text-bold Nota. 
+            | Sevilla, E., Ortiz, F., & López, M. (2013)
+                   
+    .row.justify-content-center.align-items-center.mb-5
+      .col-lg-8.mb-3
+        .cajon.color-acento-contenido.p-4.ps-5.bg_18.mb-5.mb-lg-0
+          p.m-0 Pero, así como se obtienen beneficios mediante la aplicación de sistemas de trazabilidad, también las empresas, los consumidores y los entes de control tiene responsabilidades y deben cumplir obligaciones, siendo necesario el sistema de trazabilidad obtenga, procese y recupere información sobre los productos, materias primas, procesos y en general de todas las actividades que se realicen y estén bajo su dominio y así lograr identificar adecuadamente los productos y obtener la trazabilidad a lo largo de toda la cadena.
+      .col-lg-4
+        figure
+          img.img-t(src='@/assets/curso/images/tema3/img_1.png', alt='')
+  
+    .row.justify-content-center.align-items-center.mb-5
+      .col-lg-10
+        .titulo-sexto.color-acento-contenido.mb-3
+          h5 Figura 9.
+          span Responsabilidades en la trazabilidad
+        figure
+          img.img-t.mb-2(src='@/assets/curso/images/tema6/img_6.svg', alt='')
+          figcaption 
+            span.text-bold Nota. 
+            | Sevilla, E., Ortiz, F., & López, M. (2013)
     
+    .row.mb-4
+      .col-auto
+        img(src='@/assets/curso/images/tema5/img_7.svg' , alt='')
+      .col-lg-6
+        h3.mt-4(data-aos="fade-right") Análisis de peligros y puntos críticos de control
+    
+    .row.justify-content-center.align-items-center.mb-5
+      p Se debe tener claro que la calidad e inocuidad de los alimentos es responsabilidad del productor, lo que conlleva a la implementación y diseño del HACCP o análisis de peligros y puntos críticos de control, ya que este sistema permite emplear medidas preventivas en las etapas donde se presenten peligros y así garantizar la inocuidad mediante controles eficaces y efectivos.
+      .col-lg-5
+        figure
+          img.img-t(src='@/assets/curso/images/tema3/img_1.png', alt='')
+      .col-lg-7.mb-3
+        p Además de brindar beneficios indirectos relacionados con la disminución de costos por reprocesos, destrucción o retirar del mercado productos por razones de seguridad, ya que es una herramienta enfocada en la prevención de peligros para establecer controles; y su vez se aumenta la confianza de los consumidores porque están adquiriendo productos inocuos y seguros. 
+        .tarjeta.color-secundario-claro-sin-bordes.p-3.ps-4.bg_18.mb-4.mb-lg-0
+          .row
+            .col-md-2
+              img.mt-3.img-t.m-auto(src='@/assets/curso/images/tema3/img_2.svg', alt='')
+            .col-md-10
+              p.mt-3.p-1 Como indica Torres, E. G., Matos, A. R., Fernández, M. O., & Sánchez, O. M. (2005), el sistema HACCP mediante un acompañamiento, seguimiento detallado, con el empleo de técnicas y medidas de monitoreo constante se logra tener control del proceso y de los puntos críticos y peligros potenciales que afectan la inocuidad. 
+          
+      .col-md-12
+        .cajon.color-acento-botones.p-4.ps-5.bg_18.mb-5.mb-lg-0
+          p.m-0 Debido a que el sistema HACCP se fundamente en una secuencia fases que están interrelacionadas empleadas en todos las etapas y eslabones de la cadena de producción, es decir desde la producción primaria hasta la comercialización y de acuerdo con el decreto 60 de 2002, en el artículo 4 establece que el sistema HACCP se fundamenta en la aplicación de 7 principios:
+    
+    .row.justify-content-center.align-items-center.mb-5
+      .col-lg-12
+        .titulo-sexto.color-acento-contenido.mb-3
+          h5 Figura 10.
+          span Principios HACCP
+        figure
+          img.img-t.mb-2(src='@/assets/curso/images/tema6/img_6.svg', alt='')
+          figcaption 
+            span.text-bold Nota. 
+            | Sistema de análisis de peligros y de puntos críticos de control (HACCP) y directrices para su aplicación. (2021, febrero 9).
+      
   </template>
 
 <script>
